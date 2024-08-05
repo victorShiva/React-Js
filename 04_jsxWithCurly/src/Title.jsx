277cbfb72c3987b24c08ceed3db734ccfcf097c4
shiva gupta <shivagupta5607@gmail.com>
2024-08-05 20:10:14 +0530

    import CSS file and set variable and create component

---
+++ 04_jsxWithCurly/src/Title.jsx
@@ -0,0 +1,11 @@
+function Title() {
+  let username = "Shiva";
+  return (
+    <div>
+      <h2>This Is Greeting For MSG</h2>
+      <p>Hey Good Morning {username.toUpperCase()}</p>
+    </div>
+  );
+}
+
+export default Title;
